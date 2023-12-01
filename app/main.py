e9c21d0ae76019dceec5eacb1f9416c6a338a2a6
Daniel Douek <dadouek@gmail.com>
2023-12-01 14:37:58 +0200

    get status by host id
    add host_id to the run request and store the runner
    add a status endpoint that get the status from the runner of the host_id

--- app/main.py
+++ app/main.py
@@ -1,19 +1,30 @@
+from .runner import AnsibleRunner
 from fastapi import FastAPI
 from pydantic import BaseModel
 
 app = FastAPI()
 
+runner = AnsibleRunner()
+
+
 class PlaybookParams(BaseModel):
     role: str
     host: str
     password: str
+    host_id: str
+
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World11"}
+
 
 @app.post("/run/")
-async def run(params :PlaybookParams):
-    from .runner import AnsibleRunner
-    runner = AnsibleRunner()
-    runner.run_playbook(params.host, params.role, params.password)
+async def run(params: PlaybookParams):
+    runner.run_playbook(params.host, params.role,
+                        params.password, params.host_id)
+
+
+@app.get("/status/{host_id}")
+async def status(host_id):
+    return runner.get_runner_status(host_id)
